85224fd8beb11c83609468bb6f91dbd0eb94622b
Pathum Palawaththa <pathumpalawaththa1998@gmail.com>
2023-12-10 14:42:23 +0530

    Add comment

--- Example.java
+++ Example.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
-class Example{
+class Example {
 
-    public static int[] insert(int[] array,int number){
-        int[] temp=new int[array.length+1];
-        for(int i=0;i<array.length;i++){
-            temp[i]=array[i];
+    public static int[] insert(int[] array, int number) {
+        int[] temp = new int[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            temp[i] = array[i];
         }
-        temp[temp.length-1]=number;
+        temp[temp.length - 1] = number;
         return temp;
     }
-    public static void main(String [] arg){
-        
-        int[] ar=new int[0];
-        
-        
+
+    public static void main(String[] arg) {
+
+        int[] ar = new int[0];
+
     }
-}
+}// gfhfhfj
